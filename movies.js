3c92c89b1a17345ffef8fc7f68562f60bfe804b1
Rua <rua.elkasheef@gmail.com>
2024-02-22 20:51:22 -0500

    located API for movies I wanted to use

---
+++ movies.js
@@ -0,0 +1,5 @@
+// API KEY === 3e685048
+
+// MOVIES API === https://www.omdbapi.com/?i=tt3896198&apikey=3e685048
+// Send all data requests to: http://www.omdbapi.com/?apikey=[yourkey]&
+// Poster API requests: http://img.omdbapi.com/?apikey=[yourkey]&
