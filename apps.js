648a6a9e535d1e45332d54bfae249e3f097a574e
Rua <rua.elkasheef@gmail.com>
2024-02-22 20:36:32 -0500

    starting to plan javascript file

--- apps.js
+++ apps.js
@@ -1,7 +1,29 @@
-function submit(event) {
-    event.preventDefault();
-    const loading = document.querySelector(".spinner__overlay--loading");
-    loading.classList += " spinner__overlay--visible"
-    setTimeout(() => {
-    }, 500);
- }
+// THINGS NEED TO BE DONE
+// - routing from the first page to the search page
+// window.location.href = ` URL HERE `
+// window.location.href = `${window.location.origin}/user.html`
+
+
+// - get information from the local storage
+// localStorage.setItem("id", id) --> index.js
+// localStorage.getItem("id") --> movies.js
+
+// - log the search event
+// <input type="text" placeholder="1" onchange="onSearchChange(event)">
+// function onSearchChange(event){
+//    console.log(event.target.value)
+// }
+
+
+
+
+
+
+
+// function submit(event) {
+//     event.preventDefault();
+//     const loading = document.querySelector(".spinner__overlay--loading");
+//     loading.classList += " spinner__overlay--visible"
+//     setTimeout(() => {
+//     }, 500);
+//  }
