fe70b198b0dc98c3c18995566755e7f4b97daba2
Rua <rua.elkasheef@gmail.com>
2024-02-25 21:01:07 -0500

    adding event listener for button

--- movies.js
+++ movies.js
@@ -3,3 +3,12 @@
 // MOVIES API === https://www.omdbapi.com/?i=tt3896198&apikey=3e685048
 // Send all data requests to: http://www.omdbapi.com/?apikey=[yourkey]&
 // Poster API requests: http://img.omdbapi.com/?apikey=[yourkey]&
+
+
+
+
+document.querySelector(" .redirect")
+    .addEventListener('click', () => {
+        window.location.href = `http://127.0.0.1:5501/index.html`;
+    console.log('click')
+    });
